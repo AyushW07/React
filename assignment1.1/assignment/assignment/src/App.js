09fdf0857ec5fecef1a13e765e293786c8f8d995
aayush <ayushwasnik999@gmail.com>
2023-04-20 00:45:37 +0530

    Assignment 1 Part2

---
+++ assignment1.1/assignment/assignment/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import ReactLogo from './Components/reactlogo';
+import ReactBenefit from './Components/reactbenefits'
+import Button from './Components/button';
+
+
+function App() {
+  return (
+      <div>
+        <ReactLogo /> 
+        <ReactBenefit/>   
+        <Button/>
+      </div>
+  );
+}
+
+export default App;
---
+++ assignment1.1/assignment/assignment/src/Components/reactbenefits.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ReactBenefit = () => {
+    return(
+        <div>
+            <h1>React Benefits</h1>
+            <ol type = "1">
+                <li>Component-based Architecture</li>
+                <li>Virtual DOM for efficient updates</li>
+                <li>Rich ecosystem and community</li>
+                <li>Cross-platform development</li>
+                <li>Strong community support</li>
+            </ol>
+        </div>
+    );
+};
+export default ReactBenefit
