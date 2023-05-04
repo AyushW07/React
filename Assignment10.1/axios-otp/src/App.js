6a23a785d90534d9de9bfddc396339490b3c44f6
aayush <ayushwasnik999@gmail.com>
2023-05-05 01:47:27 +0530

    Fetching OTP by Axios

---
+++ Assignment10.1/axios-otp/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import OTPGenerator from './Components/atom/OtpGenerator/OtpGenerator';
+
+function App() {
+  return (
+    <div className="App">
+      <OTPGenerator/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Assignment10.1/axios-otp/src/Components/atom/OtpGenerator/OtpGenerator.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import "./OtpGenerator.css";
+import axios from "axios";
+
+export default function OTPGenerator() {
+  const [mobile, setMobile] = useState("");
+  const [error, setError] = useState("");
+
+  const generateOTP = () => {
+    const isValidMobile = /^[6-9]\d{9}$/.test(mobile);
+    if (isValidMobile) {
+      axios
+        .post("https://cdn-api.co-vin.in/api/v2/auth/public/generateOTP", {
+          mobile: mobile,
+        })
+        .then((response) => {
+          if (response.status === 200) {
+            setError("");
+            setMobile("");
+            throw Error("OTP sent successfully");
+          } else {
+            throw new Error("Failed to send OTP");
+          }
+        })
+        .catch((error) => {
+          setError(error.message);
+        });
+    } else {
+      setError("Invalid mobile number");
+    }
+  };
+
+  return (
+    <div>
+      <h1>OTP Generator</h1>
+      <input
+        type="number"
+        placeholder="Enter mobile number"
+        value={mobile}
+        onChange={(e) => setMobile(e.target.value)}
+        className="input"
+      />
+      <button onClick={generateOTP} className="button">
+        Generate OTP
+      </button>
+      {error && <div>{error}</div>}
+    </div>
+  );
+}
